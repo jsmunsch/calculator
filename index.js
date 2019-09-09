5e12c7f6e6e5addd6d36ef8c1cc4fccd92d6b948
Jonas Munsch <jonas1.munsch@web.de>
2019-09-09 17:48:30 +0200

    finish javascript, calculator works

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 const numberButtons = document.querySelectorAll(".number-button");
 const numberInput = document.querySelector("#manual-inputfield");
 const resultField = document.querySelector("#result-input");
-
-function addNumber() {
-  numberInput.value = number;
-}
+const operatorButtons = document.querySelectorAll(".operator-button");
+const result = numberInput.value;
 
 numberButtons.forEach(button => {
   button.addEventListener("click", () => {
     const number = button.innerHTML;
-    numberInput.value = number;
+    numberInput.value += number;
   });
 });
 
-const result = {
-  value: ""
-};
+operatorButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    if (resultField.value === "") {
+      resultField.value = numberInput.value;
+      numberInput.value = "";
+      return result;
+    } else if (button.innerHTML === "*") {
+      // const operator = button.innerHTML;
+      resultField.value = multiply();
+      numberInput.value = "";
+    } else if (button.innerHTML === "/") {
+      // const operator = button.innerHTML;
+      resultField.value = divide();
+      numberInput.value = "";
+    } else if (button.innerHTML === "+") {
+      // const operator = button.innerHTML;
+      resultField.value = add();
+      numberInput.value = "";
+    } else if (button.innerHTML === "-") {
+      // const operator = button.innerHTML;
+      resultField.value = substract();
+      numberInput.value = "";
+    }
+  });
+});
 
-function setNumber1(value) {
-  numberInput = value;
+function multiply() {
+  return resultField.value * numberInput.value;
+}
+
+function divide() {
+  return (resultField.value / numberInput.value).toFixed(2);
+}
+
+function add() {
+  return Number(resultField.value) + Number(numberInput.value);
+}
+
+function substract() {
+  return resultField.value - numberInput.value;
 }
